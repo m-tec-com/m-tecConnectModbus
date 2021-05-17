c8c401cc60ec50a25179fdb8914af043ecea58f7
Paul Richter <paul@richter.dev>
2021-05-17 10:02:35 +0200

    removed keepAlive output from UI

--- libraries/Python/example.py
+++ libraries/Python/example.py
@@ -4,18 +4,17 @@ pump = mtecConnectModbus("01");
 def connect():
     #pump.serial_port = '/dev/cu.usbmodem1431201'
     pump.serial_port = 'COM3'
+    pump.settings_keepAlive_callback = updatedValue
     pump.connect()
     
 def stop():
-    pump.stop()
+    pump.speed = 0
     
 def changeSpeed(newSpeed):
     pump.speed = int(newSpeed)
     
 def updatedValue(newValue):
-    print(newValue)
-    newText = "Output: " + str(newValue/100) + "Hz"
-    speedLabel.config(text=newText) 
+    print(str(newValue/100) + "Hz") 
 
 def changeKeepAlive():
     if(keepAliveVar.get() == 1):
@@ -35,11 +34,8 @@ connectButton.pack()
 stopButton = tkinter.Button(master, text="Stop", command=stop)
 stopButton.pack()
 
-slider = tkinter.Scale(master, from_=0, to=100, orient=tkinter.HORIZONTAL, command=changeSpeed, label="Input: ")
+slider = tkinter.Scale(master, from_=-100, to=100, orient=tkinter.HORIZONTAL, command=changeSpeed, label="Input: ")
 slider.pack()
-
-speedLabel = tkinter.Label(master, text="Output: Hz")
-speedLabel.pack()
 
 keepAliveVar = tkinter.IntVar(value=1)
 keepAlive = tkinter.Checkbutton(master, text='keepAlive',variable=keepAliveVar, onvalue=1, offvalue=0, command=changeKeepAlive)
